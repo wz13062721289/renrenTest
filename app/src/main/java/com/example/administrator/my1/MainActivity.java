4f1093602576e469bb92665ceefa36b663cb37c3
wangzan <zan.wang@renrne-inc.com>
2017-07-04 14:53:48 +0800

    1.2017年7月4日14:53:41

--- app/src/main/java/com/example/administrator/my1/MainActivity.java
+++ app/src/main/java/com/example/administrator/my1/MainActivity.java
@@ -2,7 +2,7 @@ package com.example.administrator.my1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+//wz 2017年7月4日11:37:55  
 public class MainActivity extends AppCompatActivity {
 //
 
@@ -11,5 +11,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 }
